094b2970c613d04b76905e6073adba9cfdbb071a
Ahmed Mahmoud <86148334+AhmedmMhmoud2001@users.noreply.github.com>
2022-06-19 15:05:24 +0200

    Add files via upload

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 /* Global Variables */
-const form = document.querySelector('.app__form');
+const app__form = document.querySelector('.app__form');
 const icons = document.querySelectorAll('.entry__icon');
 
 // Base URL and API Key for OpenWeatherMap API
@@ -10,40 +10,40 @@ const apiKey = '&appid=5beb43e13dc3fa8be428191e6e50a00c';
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
-// Event listener to add function to existing HTML DOM element
-document.getElementById('generate').addEventListener('click', performAction);
+// when click on generate
+const generate = document.getElementById('generate');
+generate.addEventListener('click', performAction);
 
-/* Function called by event listener */
+/* Function performAction  */
 function performAction(e) {
   e.preventDefault();
-  // get user input values
-  const newZip = document.getElementById('zip').value;
+  const codeZip = document.getElementById('zip').value;
   const content = document.getElementById('feelings').value;
-
-  getWeather(baseURL, newZip, apiKey)
+  getWeather(baseURL, codeZip, apiKey)
     .then(function (userData) {
-      // add data to POST request
-      postData('/add', { date: newDate, temp: userData.main.temp, content })
-    }).then(function (newData) {
-      // call updateUI to update browser content
-      updateUI()
-    })
-  // reset form
+      postData('/add', { 
+        date: newDate,
+        temp: userData.main.temp,
+        content 
+      })
+    }).then(function (newData) {updateUI()})
   form.reset();
 }
 
 /* Function to GET Web API Data*/
-const getWeather = async (baseURL, newZip, apiKey) => {
-  // res equals to the result of fetch function
-  const res = await fetch(baseURL + newZip + apiKey);
+const getWeather = async (baseURL, codeZip, apiKey) => {
+  const res = await fetch(baseURL + codeZip + apiKey);
   try {
-    // userData equals to the result of fetch function
     const userData = await res.json();
     return userData;
   } catch (error) {
-    console.log("error", error);
+    console.log("error 404", error);
   }
 }
+
+
+
+
 
 /* Function to POST data */
 const postData = async (url = '', data = {}) => {
@@ -70,18 +70,20 @@ const postData = async (url = '', data = {}) => {
 };
 
 
+
+
+
+
+/* Function to GET Project Data */
 const updateUI = async () => {
   const request = await fetch('/all');
   try {
     const allData = await request.json()
-    // show icons on the page
-    icons.forEach(icon => icon.style.opacity = '1');
-    // update new entry values
     document.getElementById('date').innerHTML = allData.date;
     document.getElementById('temp').innerHTML = allData.temp;
     document.getElementById('content').innerHTML = allData.content;
   }
   catch (error) {
-    console.log("error", error);
+    console.log("error404", error);
   }
 };
